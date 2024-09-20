0776720bb113c4263f66ff50e4d8c961ceed9340
biosnod <biosnod@gmail.com>
2024-09-20 19:32:33 +0300

    Make models folder portable inside project instead of user cache folder

--- python/start.microphone.bat
+++ python/start.microphone.bat
@@ -1,3 +1,23 @@
+REM ::@echo off
+
+:: Store the original directory and variables
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+:: Check for administrative rights
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell start -verb runas "%~dpnx0"
+    exit /b
+)
+
+:: Make portable models folder instead of system disk
+set myDIR="C:\Users\%USERNAME%\.cache"
+rmdir /S /Q "%myDIR%\vosk"
+IF not exist "%myDIR%" (mkdir "%myDIR%")
+mklink /D "%myDIR%\vosk" "%cd%\models\vosk"
+
 call venv-win\Scripts\activate.bat
 cd example
 python test_microphone.py
