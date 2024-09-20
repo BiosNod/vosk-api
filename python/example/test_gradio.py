9f44af60f1b1c21c86513395d03ca0743ff846b0
biosnod <biosnod@gmail.com>
2024-09-20 19:54:51 +0300

    Fix gradio server bugs and errors

--- python/example/test_gradio.py
+++ python/example/test_gradio.py
@@ -5,28 +5,32 @@ import gradio as gr
 
 from vosk import KaldiRecognizer, Model
 
-model = Model(lang="en-us")
+model = Model(lang="ru")
+# model = Model(lang="en-us")
 
 def transcribe(stream, new_chunk):
+    if new_chunk:
+        sample_rate, audio_data = new_chunk
+        audio_data = audio_data.tobytes()
+        rec = None
 
-    sample_rate, audio_data = new_chunk
-    audio_data = audio_data.tobytes()
+        rec, result = stream or (None, None)
+        if rec is None:
+            rec = KaldiRecognizer(model, sample_rate)
+            result = []
 
-    if stream is None:
-        rec = KaldiRecognizer(model, sample_rate)
-        result = []
+        if rec.AcceptWaveform(audio_data):
+            text_result = json.loads(rec.Result())["text"]
+            if text_result != "":
+                result.append(text_result)
+            partial_result = ""
+        else:
+            partial_result = json.loads(rec.PartialResult())["partial"] + " "
+
+        return (rec, result), "\n".join(result) + "\n" + partial_result
     else:
-        rec, result = stream
-
-    if rec.AcceptWaveform(audio_data):
-        text_result = json.loads(rec.Result())["text"]
-        if text_result != "":
-            result.append(text_result)
-        partial_result = ""
-    else:
-        partial_result = json.loads(rec.PartialResult())["partial"] + " "
-
-    return (rec, result), "\n".join(result) + "\n" + partial_result
+        print("Empty new_chunk, skip it")
+        return (None, None), ""
 
 gr.Interface(
     fn=transcribe,
