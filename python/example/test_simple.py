4a9cb221ba285dec8ca3e35ee894100b1b7edb9b
biosnod <biosnod@gmail.com>
2024-09-21 23:06:28 +0300

    Add stereo wav sound support (convert to mono)

--- python/example/test_simple.py
+++ python/example/test_simple.py
@@ -3,16 +3,43 @@
 import wave
 import sys
 import json
+import numpy as np
+import io
 
 from vosk import Model, KaldiRecognizer, SetLogLevel
+
+
+def stereo_to_mono(audio_data):
+    return np.mean(audio_data, axis=1, dtype=audio_data.dtype)
+
 
 # You can set log level to -1 to disable debug messages
 SetLogLevel(0)
 
 wf = wave.open(sys.argv[1], "rb")
-if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
-    print("Audio file must be WAV format mono PCM.")
+if wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+    print("Audio file must be WAV format PCM.")
     sys.exit(1)
+
+# Convert to mono if stereo
+if wf.getnchannels() == 2:
+    print("Converting stereo to mono")
+    audio = np.frombuffer(wf.readframes(wf.getnframes()), dtype=np.int16)
+    audio = audio.reshape(-1, 2)
+    audio_mono = stereo_to_mono(audio)
+
+    # Create a new in-memory wave file
+    bytes_io = io.BytesIO()
+    mono_wf = wave.open(bytes_io, 'wb')
+    mono_wf.setnchannels(1)
+    mono_wf.setsampwidth(wf.getsampwidth())
+    mono_wf.setframerate(wf.getframerate())
+    mono_wf.writeframes(audio_mono.tobytes())
+    mono_wf.close()
+
+    # Reopen the in-memory file for reading
+    bytes_io.seek(0)
+    wf = wave.open(bytes_io, 'rb')
 
 #model = Model(lang="en-us")
 model = Model(lang="ru")
